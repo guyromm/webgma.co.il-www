807ef129bd65f06a402b121aa0f258b6bbfee88a
Guy Romm <guyromm@gmail.com>
2021-09-01 20:15:11 +0300

    BUILD edce1bcb-8e54-4b4e-bb90-2f07954b5b97

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1cc1411ea06dd7a6f9b3"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "2f953c85b9df271ecffc"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
