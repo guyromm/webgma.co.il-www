c5c1118b5b93827545f065e35278e6d66ba442de
Guy Romm <git-kwisatz@guyromm.com>
2023-11-09 17:56:08 +0200

    new article (scraping).

--- _next/static/commons/manifest.js
+++ _next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "f4cd71cf73016f0eaaee"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5f06eed5697fe2a69fa7"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -755,7 +755,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_document.js","3":"bundles/pages/_app.js","6":"bundles/pages/Articles/postgresql-postgrest-svelte-intro/en.js","9":"bundles/pages/OpenSource.js"}[chunkId]||chunkId) + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"main.js","1":"bundles/pages/_error.js","2":"bundles/pages/_document.js","3":"bundles/pages/_app.js","5":"bundles/pages/Articles/web-scraping-like-a-10x-dev/en.js","6":"bundles/pages/Articles.js"}[chunkId]||chunkId) + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
